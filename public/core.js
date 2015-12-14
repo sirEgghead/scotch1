be6ad71edd8c4b5237ef42a310d7c9ed96b6c434
sirEgghead <the1337bofh@gmail.com>
2015-12-14 18:30:20 -0500

    added CRUD to core.js

---
+++ public/core.js
@@ -0,0 +1,36 @@
+var scotchTodo = angular.module('scotchTodo', []);
+
+function mainController($scope, $http){
+  $scope.formData = {};
+  
+  $http.get('api/tools')
+    .success(function(data){
+      $scope.todos = data;
+      console.log(data);
+    })
+    .error(function(data){
+      console.log('Error: ' + data);
+    });
+    
+  $scope.createTodo = function(){
+    $http.post('/api/todos', $scope.formData)
+      .success(function(data){
+        $scope.formData = {};
+        $scope.todos = data;
+        console.log(data);
+      })
+      .error(function(data){
+        console.log('Error: ' + data);
+      })
+  };
+  $scope.deleteTodo = function(){
+    $http.delete('/api/todos' + id)
+      .success(function(data){
+        $scope.todos = data;
+        console.log(data);
+      })
+      .error(function(data){
+        console.log('Error: ' + data);
+      });
+  };  
+}
